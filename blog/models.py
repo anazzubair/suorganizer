d307536042c91722b3a70cc23f23fb8718e05592
Anaz Zubair <anazzubair@yahoo.com>
2016-02-20 21:24:42 +0400

    Set up the model

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,20 @@ from organizer.models import Tag, Startup
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=63,
+                            unique=True,
+                            help_text='A lable for URL config',
+                            unique_for_month='pub_date')
     text = models.TextField()
-    pub_date = models.DateField()
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
+    pub_date = models.DateField('date published',
+                                auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
 
+    def __str__(self):
+        return "{} on {}".format(self.title, self.pub_date.strftime('%Y-%m-%d'))
 
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = ['pub_date']
